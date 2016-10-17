071417b5bdf598f4b19148110f644a63a986c67f
Musa Ali <Musali94@gmail.com>
2016-10-16 20:22:06 -0400

    Commit for lab05

---
+++ Lab05/Lab0502/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab0502
+//
+//  Created by Khadija Ali on 10/5/16.
+//  Copyright (c) 2016 Musa Ali. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
